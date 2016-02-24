29dab8710dfde12b537570698e2f142187601915
G Sagoo <aran9973@gmail.com>
2016-02-24 00:34:19 +0000

    Especially designed for QPython3 on phones

---
+++ weeks2examphone.py
@@ -0,0 +1,15 @@
+import re
+from datetime import date
+
+rdir="/storage/emulated/0/com.hipipal.qpyplus/mycode/"
+with open(rdir+'examdates.txt','r') as f:
+    for line in f:
+        mystr = re.findall(r"\d{1,2}[-|\/]\d{1,2}[-|\/]\d{2,4}",line)
+        examname = re.findall(r"^\w*",line)
+        if(len(mystr) > 0 and len(examname) > 0):            
+            arraydate=re.split(r"[-|\/]", mystr[0])
+            dateholder=date(int(arraydate[2]), int(arraydate[1]), int(arraydate[0]))
+            difference = dateholder-date.today()
+            print(examname[0],dateholder,' ',int(difference.days/7), "weeks &",difference.days%7,"days\n")          
+
+
